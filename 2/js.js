bbbfe7ac4f2b3ca83e8c353b675b1e01820240c4
1karen3 <110558085+1karen3@users.noreply.github.com>
2022-12-16 22:24:03 +0200

    massive

--- 2/js.js
+++ 2/js.js
@@ -11,32 +11,30 @@ const products = [
   const DISCOUNT_START_FROM = 10000;
   const DISCOUNT = 20;
   
-  for (let prodId = 0; prodId < products.length; prodId++) {
-    console.log(`${prodId + 1} ${products[prodId].name} ${products[prodId].price}`);
-  }
-  // Условия на наличие товара
+  function showProducts () {
+    for (let prodId = 0; prodId < products.length; prodId++) {
+        console.log(`${prodId + 1} ${products[prodId].name} ${products[prodId].price}`);
+      }
+    }
   function conditionNum(num) {
     return num < 1 || num > products.length;
   }
-  // Условия на количество товара
   function conditionCount(num) {
     return num <= 0;
   }
-  // Функция запрашивает данные у клиента и проверяет их
   function answerClient(textAnswer, condition) {
     let answer;
     do {
-      answer = Number(prompt(textAnswer)); // Сразу преобразуем к числу
+      answer = Number(prompt(textAnswer));
       if (answer === null) {
         break;
       }
     } while (condition(answer))
     return answer;
   }
-  // Получаем товар
+  showProducts();
   const product = products[answerClient('Write product number:', conditionNum) - 1];
   console.log(product);
-  // Получаем количество
   const prodCount = answerClient('Write product count:', conditionCount);
   
   const totalProdPrice = prodCount * product.price;
@@ -46,3 +44,4 @@ const products = [
     console.log(`Congrats, you get a discount ${DISCOUNT}%`);
     console.log(`Your total price is: ${totalProdPrice * (100 - DISCOUNT) / 100}`);
   }
+
